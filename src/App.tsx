2446bf12efb9397088dc1f09b45c5ab39d17cfd9
mrandyyp <32210724+mrandyyp@users.noreply.github.com>
2025-12-12 09:56:08 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import WeddingInvitation from './components/WeddingInvitation';
+import GuestCommentsPage from './components/GuestCommentsPage';
 
 function App() {
   return (
     <Routes>
+      <Route path="/ucapan" element={<GuestCommentsPage />} />
       <Route path="/:guestName" element={<WeddingInvitation />} />
       <Route path="/" element={<Navigate to="/tamu-undangan" replace />} />
     </Routes>
